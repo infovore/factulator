4548f82a7a373ca0887b6ed52d3ac8e485e7abe9
Tom Armitage <tom@infovore.org>
2011-09-10 20:41:17 +0100

    Using a database to store podcasts.

--- factulator.rb
+++ factulator.rb
@@ -1,6 +1,9 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
+require 'sequel'
+
+DB = Sequel.sqlite('factulator.db')
 
 index_page_url = "http://www.factmag.com/category/factmixes/"
 
@@ -8,16 +11,18 @@ doc = Nokogiri::HTML(open(index_page_url))
 
 pages = doc.css("a.archiveTitle")
 
-mp3s = []
-
 pages.each do |page|
   page_url = page.attr("href")
   page_doc = Nokogiri::HTML(open(page_url))
   mp3_links = page_doc.css("a").select {|link| link.attr("href").match("mp3.factmagazine.co.uk")}
   if mp3_links.any?
     mp3 = mp3_links.first
-    mp3s << {:title => mp3.inner_text, :url => mp3.attr("href"), :page_url => page_url}
+    title = mp3.inner_text
+    url = mp3.attr('href')
+
+    unless DB[:podcasts].where(:url => url).any?
+      DB[:podcasts].insert(:title => title, :url => url, :page_url => page_url)
+    end
   end
 end
 
-p mp3s
---
+++ migration.rb
@@ -0,0 +1,15 @@
+require 'rubygems'
+require 'sequel'
+
+DB = Sequel.sqlite('factulator.db')
+
+DB.create_table :podcasts do
+  primary_key :id
+  String :title, :null => false
+  String :url, :null => false
+  String :page_url, :null => false
+  TrueClass :active, :default => true
+  DateTime :created_at
+  index :created_at
+end
+
